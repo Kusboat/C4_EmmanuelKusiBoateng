e8d6c2008f62fe53f3db49bf083572e089efa9d4
Kusboat <kusboat@gmail.com>
2023-11-10 08:53:08 +0000

    Modified

--- hello world/src/App.jsx
+++ hello world/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
  function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <><h1>Hello World</h1>
-    <p>I am Emmanuel Kusi Boateng, this is my first React application.</p></>  
+    <p><b>Name:</b> Emmanuel Kusi Boateng </p>
+    <p><b>Education:</b> Bachelors Degree </p>
+    <p><b>Profesion:</b> Telecommunication Engineer </p>
+    <p><b>Skills:</b></p>
+    <p><i>Networking, Cybersecurity, Frontend Development.</i>
+      </p></>  
   )
   
 
