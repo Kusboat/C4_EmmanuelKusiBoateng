88ff6ab36b4dfe2534f22f9afe3ba9676dcd5990
Kusboat <kusboat@gmail.com>
2023-11-13 21:06:27 +0000

    Assignment 3

--- hello world/src/App.jsx
+++ hello world/src/App.jsx
@@ -1,22 +1,15 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button.jsX'
 
- function App() {
-  //const [count, setCount] = useState(0)
+function App() {
+
 
   return (
-    <><h1>Hello World</h1>
-    <p><b>Name:</b> Emmanuel Kusi Boateng </p>
-    <p><b>Education:</b> Bachelors Degree </p>
-    <p><b>Profesion:</b> Telecommunication Engineer </p>
-    <p><b>Skills:</b></p>
-    <p><i>Networking, Cybersecurity, Frontend Development.</i>
-      </p></>  
+    <>
+    <Button name="Signin"/>
+    <Button name="Login"/>
+    </>
   )
-  
-
 }
 
 export default App
